2228e76700c3619b7cc0eebfd426bd097f1676f5
śhanhaider23 <shan.haider@gmail.com>
2021-02-13 18:13:08 +0100

    implemented the changes

--- javascript/javascript1/week4/command.js
+++ javascript/javascript1/week4/command.js
@@ -2,68 +2,81 @@
 
 let inPutName = "";
 let toDoList = [];
-function getReply(command){
-   
-      let inputValue = command.split(" ") 
-      if (command.includes("Hello my name is ")) {
-        if ( inPutName===  inputValue[ inputValue.length - 1]) {
-            return `Your are already registered.`
-        } else {
-            inPutName = inputValue[ inputValue.length - 1];
-            return `Nice to meet you ${inPutName}.`;
-        }
-          }  if (command === "What is my name") {
-             if (!inPutName) {
-             return `your name is not registered yet`;
-        }   else {
-            return `Your name is ${inPutName}.`;
-        }
+function getReply(command) {
+  let inputValue = command.split(" ");
+  if (command.includes("Hello my name is ")) {
+    if (inPutName === inputValue[inputValue.length - 1]) {
+      return `Your are already registered.`;
+    } else {
+      inPutName = inputValue[inputValue.length - 1];
+      return `Nice to meet you ${inPutName}.`;
     }
-          if (command === "Add fishing to my todo") {
-          toDoList.push("Fishing");
-          return `Fishing added to my todo`;
+  }
+  if (command === "What is my name") {
+    if (!inPutName) {
+      return `your name is not registered yet`;
+    } else {
+      return `Your name is ${inPutName}.`;
     }
-          if (command === "Add singing in the shower to my todo") {
-          toDoList.push("singing in the shower");
-          return `Singing during shower added to my todo`;
+  }
+  if (inputValue[0] === "Add") {
+    toDoList.push(inputValue.slice(1, inputValue.indexOf("to")).join(" "));
+    return (
+      inputValue.slice(1, inputValue.indexOf("to")).join(" ") +
+      " added to my todo"
+    );
+  }
+
+  if (command === "Remove fishing from my todo") {
+    let fishingIndex = toDoList.indexOf("fishing");
+    if (fishingIndex < 0) {
+      return `Item is missing in list.`;
     }
-    
-        if (command === "Remove fishing from my todo") {
-            let fishingIndex = toDoList.indexOf("Fishing");
-            if (fishingIndex < 0) {
-                return `Item is missing in list.`;
-            }
-            toDoList.splice(fishingIndex, 1);
-            return `Removed fishing from my todo`;
-
-        }
-            if (command === "What is on my todo?") {
-            const inPut = toDoList.join(" and ")
-            return `You have ${toDoList.length} on your todo list: ${inPut}`;
-        }
-            if(command ==="What day is it today?") {
-            let monthsOfYear = ["January","Febrauary","March","April","May","June","July","August","September","October","November","December"];
-            let month = new Date().getMonth();
-            month = monthsOfYear[month];
-            return `${new Date().getDate()} of ${month} ${new Date().getFullYear()}`;
- }
-            if (/[+/%-*]/.test(command)){
-            const mathValue = command.replace(/[A-Za-z?]/gi,"");
-            return eval(mathValue);
+    toDoList.splice(fishingIndex, 1);
+    return `Removed fishing from my todo`;
+  }
+  if (command === "What is on my todo?") {
+    const inPut = toDoList.join(" and ");
+    return `You have ${toDoList.length} on your todo list: ${inPut}`;
+  }
+  if (command === "What day is it today?") {
+    let monthsOfYear = [
+      "January",
+      "Febrauary",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let month = new Date().getMonth();
+    month = monthsOfYear[month];
+    return `${new Date().getDate()} of ${month} ${new Date().getFullYear()}`;
+  }
+  if (/[+/%-*]/.test(command)) {
+    const mathValue = command.replace(/[A-Za-z?]/gi, "");
+    return eval(mathValue);
+  }
+  if (command === "Set a timer for 4 minutes") {
+    setTimeout(function () {
+      console.log("Timer done");
+    }, 4 * 60 * 1000);
+    return `Timer set for 4 minutes`;
+  }
 }
-            if(command ==="Set a timer for 4 minutes"){
-            setTimeout(function(){ console.log("Timer done"); }, (4 * 60 * 1000));
-            return `Timer set for 4 minutes`;               
-}
-}
-
-
 console.log(getReply("Hello my name is Benjamin"));
 console.log(getReply("Hello my name is Benjamin"));
 console.log(getReply("What is my name"));
 console.log(getReply("Hello my name is Shan"));
 console.log(getReply("What is my name"));
 console.log(getReply("Add fishing to my todo"));
+console.log(getReply("Add cooking dinner to my todo"));
+console.log(getReply("Add cleaning to my todo"));
 console.log(getReply("Add singing in the shower to my todo"));
 console.log(getReply("Remove fishing from my todo"));
 console.log(getReply("What is on my todo?"));
