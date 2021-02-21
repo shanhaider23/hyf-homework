8f253810f61334fccde5e30e99dcf8e2b0ab34be
śhanhaider23 <shan.haider@gmail.com>
2021-02-21 20:37:15 +0100

    Implement the changes suggested by mentor

--- javascript/javascript2/week1/main.js
+++ javascript/javascript2/week1/main.js
@@ -4,23 +4,17 @@ const products = getAvailableProducts();
 console.log(products);
 
 function renderProducts(products) {
-    let listUl = document.createElement('ul');
-    let main = document.querySelector('main');
-    let listLi;
-    main.appendChild(listUl);
-    for (let i = 0; i < products.length; i++) {
-        listLi = document.createElement('li');
-        listLi.innerHTML = `<b>${products[i].name}</b> <br>  
+  let listUl = document.createElement("ul");
+  let main = document.querySelector("main");
+  let listLi;
+  main.appendChild(listUl);
+  for (let i = 0; i < products.length; i++) {
+    listLi = document.createElement("li");
+    listLi.innerHTML = `<b>${products[i].name}</b> <br>  
             Price: ${products[i].price} <br>
             Rating: ${products[i].rating}`;
-        listUl.appendChild(listLi);
-    }
+    listUl.appendChild(listLi);
+  }
 }
 
-renderProducts(products); 
-
-
-
-
-
-
+renderProducts(products);
