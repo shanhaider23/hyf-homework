0086ff27e3134a6d32d8898f75db2071f0f38d16
śhanhaider23 <shan.haider@gmail.com>
2021-05-03 11:20:09 +0200

    changed the reamining quries

--- databases/week3/Additional_queries.sql
+++ databases/week3/Additional_queries.sql
@@ -13,7 +13,7 @@ FROM meal
 LEFT JOIN reservation
 	ON meal.id = meal_id
 GROUP BY meal.id
-HAVING  max_reservations > total_reservations is null
+HAVING  max_reservations > total_reservations 
 order by available_spots;
 
 -- Get meals that has been created between two dates
@@ -22,12 +22,12 @@ where created_date between '2020-01-01' and '2020-12-01';
 
 -- Get only specific number of meals fx return only 5 meals
 select * from meal 
-where id between 1 and 5;
+limit 5;
 
 -- Get the meals that have good reviews
-select meal.title , review.stars
+select meal.title , AVG(review.stars)
 from meal inner join review on meal.id = review.meal_id
-where stars >3 order by stars desc;
+where stars >3 group by meal_id;
 
 -- Get reservations for a specific meal sorted by created_date
 select meal.id, meal.title, reservation.created_date, reservation.meal_id
