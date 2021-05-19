dbdf722623ad08bae002e86fc2833c9e6cf03145
śhanhaider23 <shan.haider@gmail.com>
2021-05-19 19:08:42 +0200

    implemented the changes

--- nodejs/week3/homework/warmup/index.js
+++ nodejs/week3/homework/warmup/index.js
@@ -3,24 +3,34 @@ const app = express();
 
 app.get("/", (req, res) => res.send("nodejs week3 homework"));
 
-app.get('/calculator/multiply',  (req, res) => {
-    const values = Object.values(req.query).flat().reduce((acc, val) => (acc) * (val))
-    res.send(` ${values}`)
+app.get("/calculator/multiply", (req, res) => {
+  const values = Object.values(req.query)
+    .flat()
+    .reduce((acc, val) => acc * val);
+  res.send(` ${values}`);
 });
-app.get('/calculator/add',  (req, res) => {
-    const values = Object.values(req.query).flat().reduce((acc, val) => ((acc)*1 + (val)*1))
-    res.send(` ${values}`)
+app.get("/calculator/add", (req, res) => {
+  const values = Object.values(req.query)
+    .flat()
+    .reduce((acc, val) => acc * 1 + val * 1);
+  res.send(` ${values}`);
 });
-app.get('/calculator/substract',  (req, res) => {
-    const values = Object.values(req.query).flat().reduce((acc, val) => 1*(acc - (val)))
-    res.send(` ${values}`)
+app.get("/calculator/subtract", (req, res) => {
+  const values = Object.values(req.query)
+    .flat()
+    .reduce((acc, val) => 1 * (acc - val));
+  res.send(` ${values}`);
 });
-app.get('/calculator/divide',  (req, res) => {
-    const values = Object.values(req.query).flat().reduce((acc, val) => 1*((acc) / (val)))
-    res.send(` ${values}`)
+app.get("/calculator/divide", (req, res) => {
+  const values = Object.values(req.query)
+    .flat()
+    .reduce((acc, val) => 1 * (acc / val));
+  res.send(` ${values}`);
 });
-app.post('/calculator/add', (req, res) =>{
-    const values = Object.values(req.query).flat().reduce((acc, val) =>((acc)*1 + (val)*1))
-    res.send(` ${values}`)
+app.post("/calculator/add", (req, res) => {
+  const values = Object.values(req.query)
+    .flat()
+    .reduce((acc, val) => acc * 1 + val * 1);
+  res.send(` ${values}`);
 });
 app.listen(3000, () => console.log(`Calculator:listening on port 3000`));
