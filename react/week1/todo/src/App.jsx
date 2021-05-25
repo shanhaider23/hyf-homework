7890d4d3b140d21c59fa14662b85a6eee3739977
śhanhaider23 <shan.haider@gmail.com>
2021-05-25 11:13:12 +0200

    React week 1 homework final  implementation

--- react/week1/todo/src/App.jsx
+++ react/week1/todo/src/App.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import Card from "./Card";
 import list from "./todolist";
 
-function createCard(list) {
+
+function App() {
   return (
+    <div>
+      <h1 className="heading">My Todo list</h1>
+      {list.map( (list) => (
     <Card
       key={list.id}
       title={list.title}
@@ -11,13 +15,7 @@ function createCard(list) {
       date={new Date(list.date).toDateString()}
       time={list.time}
     />
-  );
-}
-function App() {
-  return (
-    <div>
-      <h1 className="heading">My Todo list</h1>
-      {list.map(createCard)}
+  ))}
     </div>
   );
 }
