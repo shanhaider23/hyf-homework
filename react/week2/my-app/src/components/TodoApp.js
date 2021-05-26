f3b72dd3427ac48573899045b4587eb303b9c093
śhanhaider23 <shan.haider@gmail.com>
2021-05-26 20:00:03 +0200

    React week2 Homework

---
+++ react/week2/my-app/src/components/TodoApp.js
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+import "./todoapp.css";
+
+function TodoApp() {
+  const [task, setTask] = useState("");
+  const [tasklist, setTaskList] = useState([]);
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => setSeconds(seconds + 1), 1000);
+  });
+  const handleChange = (e) => {
+    setTask(e.target.value);
+  };
+
+  const AddTask = () => {
+    if (task !== "") {
+      const taskDetails = {
+        id: Math.floor(Math.random() * 1000),
+        value: task,
+        isCompleted: false,
+      };
+
+      setTaskList([...tasklist, taskDetails]);
+    }
+  };
+
+  const deletetask = (e, id) => {
+    e.preventDefault();
+    setTaskList(tasklist.filter((t) => t.id !== id));
+  };
+
+  const taskCompleted = (e, id) => {
+    e.preventDefault();
+    //let's find index of element
+    const element = tasklist.findIndex((elem) => elem.id === id);
+
+    //copy array into new variable
+    const newTaskList = [...tasklist];
+
+    //edit our element
+    newTaskList[element] = {
+      ...newTaskList[element],
+      isCompleted: true,
+    };
+
+    setTaskList(newTaskList);
+  };
+
+  return (
+    <div className="todo">
+      <h2>You have used {seconds} seconds on this webside</h2>
+      <input
+        type="text"
+        name="text"
+        id="text"
+        onChange={(e) => handleChange(e)}
+        placeholder="Add task here..."
+      />
+      <button className="add-btn" onClick={AddTask}>
+        Add
+      </button>
+      <br />
+      {tasklist !== [] ? (
+        <ul>
+          {tasklist.map((t) => (
+            <li className={t.isCompleted ? "crossText" : "listitem"}>
+              {t.value}
+              <button
+                className="completed"
+                onClick={(e) => taskCompleted(e, t.id)}
+              >
+                Completed
+              </button>
+
+              <button className="delete" onClick={(e) => deletetask(e, t.id)}>
+                Delete
+              </button>
+            </li>
+          ))}
+        </ul>
+      ) : null}
+    </div>
+  );
+}
+
+export default TodoApp;
