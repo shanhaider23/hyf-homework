32d71aa809cdc143119ba30138af7374e5979dfb
śhanhaider23 <shan.haider@gmail.com>
2021-06-12 23:39:27 +0200

    immutable way of doing this is using the spread operator

--- react/week3/todoapp-w3/src/components/TodoList.js
+++ react/week3/todoapp-w3/src/components/TodoList.js
@@ -38,9 +38,8 @@ function TodoList() {
   const completeTodo = (id) => {
     let updatedTodos = todos.map((todo) => {
       if (todo.id === id) {
-        todo.isComplete = !todo.isComplete;
-      }
-      return todo;
+        return { ...todo, isComplete: !isComplete };
+      } else return todo;
     });
     setTodos(updatedTodos);
   };
